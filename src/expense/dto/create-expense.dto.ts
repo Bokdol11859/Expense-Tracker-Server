d495a117078b1c2699d3a93ee690b0c783251542
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2023-10-15 18:26:29 +0900

    [Feat] Implement Expense App (#2)
    * [Feat] initialize expense app
    
    * [Feat] Implement CRUD without db
    
    * [Feat] Implement CRUD with sqlite database
    
    * [Feat] Enable CORS
    
    * [Build] Change start script for cyclic deployment

---
+++ src/expense/dto/create-expense.dto.ts
@@ -0,0 +1,5 @@
+export class CreateExpenseDto {
+  date: string;
+  description: string;
+  amount: number;
+}
---
+++ src/expense/expense.service.ts
@@ -0,0 +1,56 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateExpenseDto } from './dto/create-expense.dto';
+import { UpdateExpenseDto } from './dto/update-expense.dto';
+import { Expense } from './entities/expense.entity';
+import { randomUUID } from 'crypto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class ExpenseService {
+  constructor(
+    @InjectRepository(Expense)
+    private expenseRepository: Repository<Expense>,
+  ) {}
+
+  async create(createExpenseDto: CreateExpenseDto): Promise<Expense> {
+    const expense = this.expenseRepository.create({
+      id: randomUUID(),
+      ...createExpenseDto,
+    });
+    await this.expenseRepository.save(expense);
+    return expense;
+  }
+
+  async findAll(): Promise<Expense[]> {
+    return this.expenseRepository.find();
+  }
+
+  async findOne(id: string): Promise<Expense> {
+    const expense = await this.expenseRepository.findOne({ where: { id } });
+    if (!expense) {
+      throw new NotFoundException(`Expense with id ${id} not found`);
+    }
+
+    return expense;
+  }
+
+  async update(
+    id: string,
+    updateExpenseDto: UpdateExpenseDto,
+  ): Promise<Expense> {
+    const expense = await this.findOne(id);
+
+    Object.assign(expense, updateExpenseDto);
+
+    await this.expenseRepository.save(expense);
+
+    return expense;
+  }
+
+  async remove(id: string): Promise<Expense[]> {
+    await this.expenseRepository.delete(id);
+
+    return this.findAll();
+  }
+}
