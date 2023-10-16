a8210ce6bcd84b40517305e25065b937df9e5bdd
Bokdol11859 <2019147551@yonsei.ac.kr>
2023-10-16 22:05:26 +0900

    [Feat] Move database from sqlite to postgres

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ExpenseModule } from './expense/expense.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Expense } from './expense/entities/expense.entity';
+import { typeORMConfig } from './configs/typeorm.config';
 
 @Module({
-  imports: [
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'expense-tracker.sqlite',
-      entities: [Expense],
-      synchronize: true,
-      logging: true,
-    }),
-    ExpenseModule,
-  ],
+  imports: [TypeOrmModule.forRoot(typeORMConfig), ExpenseModule],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/configs/typeorm.config.ts
@@ -0,0 +1,14 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import 'dotenv/config';
+
+export const typeORMConfig: TypeOrmModuleOptions = {
+  // Database Type
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT),
+  username: process.env.POSTGRES_USERNAME,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DATABASE,
+  entities: [__dirname + '/../**/*.entity.{js,ts}'],
+  synchronize: true,
+};
