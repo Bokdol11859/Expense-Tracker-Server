25cbbe9df45ed10b0ea0758e0c72f4dd445387c5
Eric Park <80627536+Bokdol11859@users.noreply.github.com>
2023-10-21 00:52:57 +0900

    [Feat] Implement login feature with access token only (#6)

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,9 +3,18 @@ import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { User } from './entities/user.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from './constant';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '60s' },
+    }),
+  ],
   controllers: [UserController],
   providers: [UserService],
 })
