8b6219ab37843a9a38dbcd0b9c23d4970a35929a
Bokdol11859 <2019147551@yonsei.ac.kr>
2023-10-21 00:50:35 +0900

    [Feat] Implement login feature with access token only

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,6 +1,7 @@
 import { Body, Controller, HttpCode, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UserService } from './user.service';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Controller('user')
 export class UserController {
@@ -10,5 +11,11 @@ export class UserController {
   @HttpCode(201)
   createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
+  }
+
+  @Post('login')
+  @HttpCode(200)
+  loginUser(@Body() loginUserDto: LoginUserDto) {
+    return this.userService.loginUser(loginUserDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,19 +1,24 @@
 import {
   ConflictException,
+  ForbiddenException,
   Injectable,
   InternalServerErrorException,
+  NotFoundException,
 } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { User } from './entities/user.entity';
 import { CreateUserDto } from './dto/create-user.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import * as bcrypt from 'bcryptjs';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private userRepository: Repository<User>,
+    private jwtService: JwtService,
   ) {}
 
   async createUser(user: CreateUserDto): Promise<Partial<User>> {
@@ -45,5 +50,27 @@ export class UserService {
     }
 
     return { firstName, lastName, email, createdAt };
+  }
+
+  async loginUser(
+    loginUserDto: LoginUserDto,
+  ): Promise<{ access_token: string }> {
+    const { email, password } = loginUserDto;
+
+    const user = await this.userRepository.findOne({ where: { email } });
+
+    if (!user) {
+      throw new NotFoundException(
+        'User with the provided credentials does not exist.',
+      );
+    }
+
+    if (!bcrypt.compare(password, user.password)) {
+      throw new ForbiddenException('Incorrect credentials provided.');
+    }
+
+    return {
+      access_token: await this.jwtService.signAsync({ email, password }),
+    };
   }
 }
